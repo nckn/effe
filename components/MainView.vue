d8ebc80925ff737cb41a1448a0ab6590c0e418ff
Niels Christian Konrad Nielsen <mail@nckn.dk>
2020-01-21 16:59:22 +0100

    Changing to sounds hosted on konradstudio.com

--- components/MainView.vue
+++ components/MainView.vue
@@ -34,8 +34,8 @@ export default {
   data () {
     return {
       players: [
-        {name: 'Player 1', type: 'player', isPlaying: false, isOn: true, vol: 0.8, id: 0, demoUrl: '/snd/effe-beat-1.wav', arrayBuffer: null},
-        {name: 'Player 2', type: 'player', isPlaying: false, isOn: true, vol: 0.8, id: 1, demoUrl: '/snd/effe-bass-1.wav', arrayBuffer: null}
+        {name: 'Player 1', type: 'player', isPlaying: false, isOn: true, vol: 0.8, id: 0, demoUrl: 'https://www.konradstudio.com/snd/effe-beat-1.wav', arrayBuffer: null},
+        {name: 'Player 2', type: 'player', isPlaying: false, isOn: true, vol: 0.8, id: 1, demoUrl: 'https://www.konradstudio.com/snd/effe-bass-1.wav', arrayBuffer: null}
       ],
       nodes: [
         {name: 'Visualizer', class_name: 'graph', isOn: true},
